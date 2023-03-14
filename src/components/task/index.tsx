982151f01c4dbda74d17c3cfe1d1e1d65685801f
Niklas P <contact@niklasplessing.net>
2023-03-15 00:45:22 +0100

    remove verbosity in task

--- src/components/task/index.tsx
+++ src/components/task/index.tsx
@@ -2,6 +2,7 @@ import React, { ChangeEvent, useEffect, useRef, useState } from "react";
 import clsx from "clsx";
 
 import styles from "./task.module.scss";
+import { string_to_slug } from "@site/src/util";
 
 export default function Task( { caption }: { caption: string } ): JSX.Element {
 
@@ -10,19 +11,29 @@ export default function Task( { caption }: { caption: string } ): JSX.Element {
 
     useEffect(() => {
         //if we are the first task component in the dom, we handle things
-        console.log( 'ref', ref )
+        // console.log( 'ref', ref )
 
-        document.querySelector('.pagination-nav__link--next')?.addEventListener( 'mouseover', () => console.log( 'yyaaaa') );
+        // document.querySelector('.pagination-nav__link--next')?.addEventListener( 'mouseenter', onNextHover );
     }, []);
 
     const onChangeInput = () => {
         setChecked(!checked)
     }
 
+    const onNextHover = () => {
+        // const tasksRemaining = 0;
+        // const values = Array
+        //     .from(document.querySelectorAll('input[type="checkbox"]'))
+        //     .filter((checkbox) => ! checkbox.checked)
+        //     .map((checkbox) => checkbox);
+        // console.log( values );
+    }
+
     return (
         <div 
             className={ clsx( 'ps-task', styles.task, checked && styles.complete ) }
             ref={ ref }
+            id={ string_to_slug( caption ) }
         >
             <label className="">
                 <input 
