3a51a8faff4bd18fb0f6614e50429273bf28c534
Niklas P <contact@niklasplessing.net>
2023-04-29 13:37:34 +0200

    style

--- src/components/three/dots.tsx
+++ src/components/three/dots.tsx
@@ -15,7 +15,7 @@ import {
   OrbitControls,
   Effects,
 } from "@react-three/drei";
-import React, { useRef, useState } from "react";
+import React, { useLayoutEffect, useMemo, useRef, useState } from "react";
 
 extend({ RenderPixelatedPass });
 
@@ -23,21 +23,43 @@ function Box(props: ThreeElements["mesh"] & { index: number }) {
   const ref = useRef<THREE.Mesh>(null!);
   const [hovered, hover] = useState(false);
   const [clicked, click] = useState(false);
-  useFrame((state, delta) => {
+
+  const pink = useMemo(() => new THREE.Color("#e6007a"), []);
+  const purple = useMemo(() => new THREE.Color("#552BBF"), []);
+  const cyan = useMemo(() => new THREE.Color("#00B2FF"), []);
+  const lime = useMemo(() => new THREE.Color("#D3FF33"), []);
+  const green = useMemo(() => new THREE.Color("#56F39A"), []);
+
+  const toColors = [purple, cyan, green];
+
+  useFrame(({ mouse, clock, viewport }, delta) => {
+    const x = (mouse.x * viewport.width) / 2.5;
+    const y = (mouse.y * viewport.height) / 2.5;
+    ref.current.lookAt(x, y, 0);
     ref.current.position.y =
-      Math.sin(state.clock.elapsedTime / 2.0 - props.index) * 0.9;
+      Math.sin(clock.elapsedTime / 2.0 - props.index) * 0.9;
+
+    ref.current.material.color.lerp(
+      hovered ? toColors[props.index] : pink,
+      0.05
+    );
+
+    ref.current.scale.lerp(
+      hovered ? new THREE.Vector3(2, 1, 1) : new THREE.Vector3(1, 1, 1),
+      0.05
+    );
   });
   return (
     <mesh
       {...props}
       ref={ref}
-      scale={clicked ? 1.5 : 1}
       onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
       onPointerOut={(event) => hover(false)}
+      scale={[1, 1, 1]}
     >
       <sphereBufferGeometry args={[0.5, 64, 64]} />
-      <meshStandardMaterial color={hovered ? "#552BBF" : "#e6007a"} />
+      <meshStandardMaterial />
     </mesh>
   );
 }
